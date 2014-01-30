b23999fe6d9666f0e50236378662abe3496786d0
Ryan Lopopolo <lopopolo@box.com>
2014-01-30 13:40:07 -0800

    add iterative bash solution

---
+++ fib-iter.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function fib
+{
+	local n="$1"
+	local a="1"
+	local b="1"
+	local temp="0"
+	while (( $n > 0 )); do
+		temp=$b
+		b=$(expr $a + $b)
+		a=$temp
+		n=$(expr $n - 1)
+	done
+	echo "$a"
+}
+
+n=$1
+if [ -z "$n" ]; then
+  n="40"
+fi
+
+fib "$n"
